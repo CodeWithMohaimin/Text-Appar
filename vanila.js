654cc2a15f858e34a767c314e8129a272af2bc85
programmermohaimin <you@example.com>
2022-08-15 07:31:35 +0600

    mastering

--- vanila.js
+++ vanila.js
@@ -10,3 +10,35 @@ function scrollAppear() {
 }
 
 window.addEventListener('scroll', scrollAppear)
+
+// this is the main code and I practice at in the bottom
+
+// function scrollAppear() {
+//     let introText = document.querySelector('.introText')
+//     let introPosition = introText.getBoundingClientRect().left
+//     let screenPosition = window.innerHeight/1.3
+
+//     if (introPosition < screenPosition) {
+//         introText.classList.add('introAppear')
+//     }
+    
+// }
+
+// window.addEventListener('scroll', scrollAppear)
+
+//another practice
+
+// this is the main code and I practice at in the bottom
+
+// function scrollAppear() {
+//     let introText = document.querySelector('.introText')
+//     let introPosition = introText.getBoundingClientRect().left
+//     let screenPosition = window.innerHeight/1.3
+
+//     if (introPosition < screenPosition) {
+//         introText.classList.add('introAppear')
+//     }
+    
+// }
+
+// window.addEventListener('scroll', scrollAppear)
